10d3b38300e1cd85c672761a5bac43113f502cc7
choim3 <choim3@spu.edu>
2021-02-05 11:31:54 -0800

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+// Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+export default class Keypad extends Component{
+
+  handleKeyUp = () => {
+    console.log("Entering password...")
+  }
+  render(){
+    return (
+      <input onKeyUp={this.handleKeyUp} type="password"/>
+    )
+  }
+}
